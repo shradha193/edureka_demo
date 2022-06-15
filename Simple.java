02cdec7274254853ed17ba1f56a293c5f685a36e
test proj <support@edureka.co>
2022-06-15 15:47:12 +0000

    Revert "Second commit of Simple.java program"
    This reverts commit c00acee6023cb5e653b7df9624194823d1398290.

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{
     public static void main(String args[]){
-    System.out.println("Hello Edureka, Welcome to Git's Online Training");
+    System.out.println("Hello World");
     }
 }
